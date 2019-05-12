166b6f32306c7473005bfe068ead93f874ca27e8
Paulius <paulius.grigaliunas.pg@gmail.com>
2019-05-12 12:35:35 +0300

    create on component state

---
+++ learn-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Main from "./Component/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This app for learning purposes</p>
+        <Main />
+      </header>
+    </div>
+  );
+}
+
+export default App;
